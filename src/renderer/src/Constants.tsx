231971ea2aaebd0987eeec1bf405a63eb370ffa8
MustafaKrc <mustafa.kirci0@gmail.com>
2024-09-16 13:59:06 +0300

    Update constants

--- src/renderer/src/Constants.tsx
+++ src/renderer/src/Constants.tsx
@@ -1,6 +1,6 @@
 // Constants.tsx
-export const VERSION = "Alpha";
+export const VERSION = "1.0.0";
 
 export const LATEST_VERSION_URL = "http://localhost:8080/version"; // URL where you check for the latest version
 
-export const APP_DOWNLOAD_URL = "https://beehub.com/download"; // URL where you download the latest version
+export const APP_DOWNLOAD_URL = "https://beehubapp.com"; // URL where you download the latest version
