c2127378ceff72c8a2b464e9082c13380c8229f7
Derviş Genç <gencd20@itu.edu.tr>
2026-02-01 16:30:47 +0300

    chore: update build scripts and dependencies in package.json

--- src/renderer/src/pages/Home.tsx
+++ src/renderer/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from "../components/ui/button";
+// import { Button } from "../components/ui/button";
 import BeakerIcon from "../components/icons/BeakerIcon";
 
 const Home: React.FC = () => {
